789527434e94652e6d3c33539d74f07615d1af58
Mauve Signweaver <RangerMauve@hotmail.com>
2024-03-13 16:13:02 -0400

    reader: Clean article wrappers before rendering

--- reader.js
+++ reader.js
@@ -1,18 +1,32 @@
+/* global DOMParser, document */
 const renderPage = require('./template.js')
-const {Readability} = require('@mozilla/readability')
+const { Readability } = require('@mozilla/readability')
 
 const article = new Readability(document).parse()
 
 const { title, content, byline } = article
 
+let cleanedContent = content
+
+try {
+  const parsed = (new DOMParser()).parseFromString(content, 'text/html')
+
+  // Find the first paragraph
+  const firstP = parsed.querySelector('p')
+  const parent = firstP.parentElement
+  cleanedContent = parent.innerHTML
+} catch (e) {
+  console.error('Unable to clean article', e)
+}
+
 const finalContent = `
-<h1>${title}</h1>
+<h1>${title || document.title}</h1>
 
 ${byline ? `<p>${byline}</p>` : ''}
 
 <hr>
 
-${content}
+${cleanedContent}
 `
 
 renderPage(finalContent, title)
